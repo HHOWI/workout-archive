ba618cab845bff6bb437fec28080819121ee2aa8
minsu <minsu705@gmail.com>
2026-04-26 00:52:54 +0900

    Monorepo structure finalized with history

--- workout-archive-fe/monorepo_fe_temp/src/api/exercise.ts
+++
@@ -1,8 +0,0 @@
-import { ExerciseDTO } from "../dtos/WorkoutDTO";
-import { publicAPI } from "./axiosConfig";
-
-// 운동 종류 목록 가져오기
-export const fetchExercisesAPI = async (): Promise<ExerciseDTO[]> => {
-  const response = await publicAPI.get("/exercises/exercises");
-  return response.data;
-};
--- workout-archive-fe/monorepo_fe_temp/src/dtos/UserInfoRecordDTO.ts
+++
@@ -1,8 +0,0 @@
-export interface UserInfoRecordDTO {
-  userInfoRecordSeq?: number;
-  userId?: String;
-  bodyWeight?: number | null;
-  muscleMass?: number | null;
-  bodyFat?: number | null;
-  recordDate?: Date;
-}
